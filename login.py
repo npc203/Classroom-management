3d22c9cb29723b3c306629936e8689f12bb2c276
npc203 <npc203@users.noreply.github.com>
2020-10-06 21:19:00 +0530

    changed db

--- login.py
+++ login.py
@@ -1,44 +1,38 @@
-import os
+import os,json
 #Helper functions
 def fetch():
     #Creating user database
-    if os.path.isfile("./users.csv"):
-        with open("./users.csv",'r') as f:
-            data = f.readlines()
+    if os.path.isfile("./users.json"):
+        with open("./users.json",'r') as f:
+            data = json.load(f)
     else:
-        with open('./users.csv','w') as f:
-            data = None
+        with open('./users.json','w') as f:
+            data = {}
     return data
+
 def login():
     data = fetch()
-    data = [i.split(",") for i in data]
     print("Enter Login credentials:")
     name = input("username:")
     password = input("password:")
-    for i in range(len(data)):
-        if name == data[i][0] :
-            if password != data[i][1]:
+    for i in data:
+        if name == i:
+            if password != data[i]["password"]:
                 print("\nInvalid Password\n")
                 return
-            if data[i][2] == 'S':
-                #print(i)
-                data[i][3] = int(data[i][3])
-                data[i][4] = int(data[i][4])
-                data[i][5] = int(data[i][5]) 
             return data[i]
     else:
         print("User not found, Please register")
-        return
 
 def register():
     data = fetch()
     print("Enter details:")
     name = input("username:")
-    password = input("password:")
     if data:
         if name in [i[0] for i in data]:
             print("User Already exists!")
             return
+    password = input("password:")
     
     while True:
         role = input("Enter Teacher or Student (T/S):")
@@ -46,16 +40,15 @@ def register():
             print("Invalid input, Try again")
         else:
             break
-    data = [name,password,role]
+    data[name] ={"name":name,"password":password,"role":role}
     if role == "S":
-        print("\nENTER YOUR MARKS:\n")
-        mat = int(input("Enter your math marks:"))
-        phy = int(input("Enter your physics marks:"))
-        che = int(input("Enter your chemistry marks:"))
-        data.append(mat)
-        data.append(phy)
-        data.append(che)
-    with open('./users.csv','a') as f:
-        temp = [str(i) for i in data]
-        f.write(','.join(temp)+'\n')
-    return data
+        data[name]["class"] = input("Enter your Class:")
+        data[name]["section"] = input("Enter your section:")
+        data[name]["roll_no"] = input("Enter your Roll No:")
+        data[name]["exam"] = input("Enter The Examination:")
+        data[name]["marks"]={}
+       
+    with open('./users.json','w') as f:
+        json.dump(data,f)
+
+    return data[name]
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-import datetime
+import os,xlwt,time
+import datetime,json
 from login import login,register
 #name,pass,role,mat,phy,che
 while True:
@@ -15,76 +15,120 @@ while True:
         if data:
             print(f"\nLogged in Time:",datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             #print(data)
-            if data[2] == 'T\n' or data[2] == 'T': #Logged as teacher
+            if data["role"] == 'T': #Logged as teacher
                 print(f"Sucessfully Logged in as Teacher\n")
                 while True:
                     print(" 1 View All student Details\n") 
                     print(" 2 View Failures\n")
                     print(" 3 Get class average\n")
                     print(" 4 View my details\n")
-                    print(" 5 Exit")
+                    print(" 5 Enter marks for student\n")
+                    print(" 6 Exit")
 
                     choice = int(input("->"))
-                    with open("./users.csv",'r') as f:
-                            all_data = f.readlines()
-                            all_data = [i.split(',') for i in all_data]
+                    with open("./users.json",'r') as f:
+                        all_data = json.load(f)
                     if choice == 1:
+                        wb = xlwt.Workbook()
+                        count=1
+                        sheet1 = wb.add_sheet("Marks")
+                        sheet1.write(0,0,"Name")
+                        sheet1.write(0,1,"Maths")
+                        sheet1.write(0,2,"Physics")
+                        sheet1.write(0,3,"Chemistry")
+                        sheet1.write(0,4,"Computer Science")
+                        sheet1.write(0,5,"English")
                         for i in all_data:
-                            if i[2]=='S':
-                                print("-----------------------------------\n")
-                                print("Name:",i[0])
-                                print("Marks:")
-                                print("Mathematics:",i[3])
-                                print("Physics    :",i[4])
-                                print("Chemistry  :",i[5])
+                            print(i)
+                            if all_data[i]["role"]=='S':
+                                i=all_data[i] #lazy to refactor
+                                sheet1.write(count,0,i["name"])
+                                sheet1.write(count,1,i["marks"]["mat"])
+                                sheet1.write(count,2,i["marks"]["phy"])
+                                sheet1.write(count,3,i["marks"]["chem"])
+                                sheet1.write(count,4,i["marks"]["comp"])
+                                sheet1.write(count,5,i["marks"]["eng"])
+                                
+                        wb.save("Student MarkSheet - "+str(time.time()).split(".")[0]+".xls")
+                        print("Sucessfully extracted into an Excel File\n")
                     elif choice == 2:
-                        for i in all_data:
-                            if i[2]=='S':
-                                if sum([int(i) for i in i[3:6]])/3 < 50:
-                                    print("-----------------------------------\n")
-                                    print("Name:",i[0])
-                                    print("Marks:")
-                                    print("Mathematics:",i[3])
-                                    print("Physics    :",i[4])
-                                    print("Chemistry  :",i[5])
+                        count=1
+                        for user in all_data:
+                            if all_data[user]["role"]=='S':
+                               user = all_data[user] #lazy to refactor
+                               for sub in user["marks"]:
+                                   if int(user["marks"][sub])<40:
+                                       print("Name:",i["name"])
+                                       count+=1
+                                       break
+                        print("Total number of failures:",count)
                     elif choice == 3:
                         count = 0
                         result = 0 
-                        for i in all_data:
-                            if i[2] == 'S':
-                                result += sum([int(i) for i in i[3:6]])/3
+                        for user in all_data:
+                            if all_data[user]["role"] == 'S':
+                                result += sum(map(int,all_data[user]["marks"].values))/5
                                 count+=1
                         print("\nTotal number of students:",count)
                         print("Class average percentage: {:.2f}%".format(result/count))
 
                     elif choice == 4:
                         print("-----------------------------------\n")
-                        print("Username:",i[0])
-                        print("Password:",i[1])
+                        print("Username:",data["name"])
+                        print("Password:",data["password"])
                         print("-----------------------------------\n")
+                    
+                    elif choice == 5:
+                        name = input("Enter the name of the student:")
+                        if name in all_data:
+                            stud=all_data[name]
+                            stud["marks"]["mat"]=input("Enter the Maths marks:")
+                            stud["marks"]["chem"]=input("Enter the Chemistry marks:")
+                            stud["marks"]["phy"]=input("Enter the Physics marks:")
+                            stud["marks"]["eng"]=input("Enter the English marks:")
+                            stud["marks"]["comp"]=input("Enter the Comp Sci marks:")
+                            all_data[name]["marks"]=stud["marks"]
+                            with open("./users.json",'w') as f:
+                                json.dump(all_data,f)
+                            print("Successfully Updated student's mark")
+                        else:
+                            print("Invalid Student Name!")
                     
                     else:
                         exit()
                     
                     input("Press enter to try again")
  
-            elif data[2] == 'S\n' or data[2] == 'S': #Logged as Student
+            elif data["role"] == 'S': #Logged as Student
                 print(f"Sucessfully Logged in as Student\n")
                 while True:
                     print(" 1 View my Details")
-                    print(" 2 Get my average")
-                    print(" 3 Exit")
+                    print(" 2 Get my marks")
+                    print(" 3 Get my average")
+                    print(" 4 Exit")
                     choice = int(input("->"))
                     if choice == 1:
                         print("-----------------------------------\n")
-                        print("Name:",data[0])
-                        print("My Marks:\n")
-                        print("Mathematics:",data[3])
-                        print("Physics    :",data[4])
-                        print("Chemistry  :",data[5])
+                        print("Name:",data["name"])
+                        print("Roll No:",data["roll_no"])
+                        print("Class   :",data["class"])
+                        print("Section  :",data["section"])
                         print("-----------------------------------\n")
                     elif choice == 2:
-                        print("My average :",sum(data[3:6])/3)
+                        if len(data["marks"])>0:
+                            print(f"My Marks in {data['exam']}:\n")
+                            print("Mathematics:",data["mat"])
+                            print("Physics    :",data["phy"])
+                            print("Chemistry  :",data["chem"])
+                            print("English    :",data["eng"])
+                            print("Comp Sci   :",data["comp"])
+                        else:
+                            print("Teacher hasn't updated your marks\n")
+                    elif choice == 3:
+                        if data["marks"]:
+                            print("My average :",sum(map(int,data["marks"].values()))/len(data["marks"]))
+                        else:
+                            print("Teacher hasn't updated your marks\n")
                     else:
                         exit()
                     
@@ -93,7 +137,7 @@ while True:
             pass
 
     except KeyError:
-        print("Invalid Choice")
+        print("Invalid Choice!!")
         exit()
         
 
